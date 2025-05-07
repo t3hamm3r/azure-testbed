95ae8e50c675ae5057c3ec4bf469d41d50bf98cf
Jacob Devereaux <jacobdev48616@gmail.com>
2025-05-06 18:52:46 -0600

    add the things plus ai

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ import os
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
-app = Flask(__name__)
+from langchain_openai import AzureOpenAI
 
+app = Flask(__name__)
+azure_endpoint = os.environ['AZURE_OPENAI_ENDPOINT']
+azure_deployment = os.environ['AZURE_OPENAI_DEPLOYMENT_NAME']
+model_name = os.environ['OPENAI_MODEL_NAME']
+api_version = os.environ['AZURE_OPENAI_API_VERSION']
+api_key = os.environ['OPENAI_API_KEY']
 
 @app.route('/')
 def index():
@@ -18,15 +24,22 @@ def favicon():
 
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
+   req = request.form.get('req')
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
+   llm = AzureChatOpenAI(
+       api_key=api_key,
+       api_version=api_version,
+       azure_deployment=azure_deployment,
+       model_name=model_name,
+   )
+   text = llm.invoke(req).content
+
+   if req:
+       print('Request for hello page received with req=%s' % req)
+       return render_template('hello.html', req=text)
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
-
 
 if __name__ == '__main__':
    app.run()
